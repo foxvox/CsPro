85c24afc5275dec50a04eab816e99389045963a3
alex <windev@nate.com>
2024-10-07 06:41:54 +0900

    Anonymous Type

--- Program.cs
+++ Program.cs
@@ -1,45 +1,21 @@
 ﻿namespace CsPro
 {
-	class CTransaction
-	{
-		public string From { get; init; }
-		public string To { get; init; }
-		public int Amount { get; init; }
-
-		public override string ToString()
-		{
-			return $"{From,-10} -> {To,-10} : ${Amount}";
-		}
-	}
-
-	record RTransaction
-	{
-		public string From { get; init; }
-		public string To { get; init; }
-		public int Amount { get; init; }
-
-		public override string ToString()
-		{
-			return $"{From,-10} -> {To,-10} : ${Amount}";
-		}
-	}
-
-
 	class Program
 	{
 		static void Main(string[] args)
 		{
-			CTransaction trA = new CTransaction { From = "Alice", To = "Bob", Amount = 100 };
-			CTransaction trB = new CTransaction { From = "Alice", To = "Bob", Amount = 100 };
-			Console.WriteLine(trA);
-			Console.WriteLine(trB);
-			Console.WriteLine($"trA equals to trB: {trA.Equals(trB)}");
+			var a = new { Name = "박상현", Age = 123 }; 
+			Console.WriteLine($"Name: {a.Name}, Age: {a.Age}");
 
-			RTransaction tr1 = new RTransaction { From = "Alice", To = "Bob", Amount = 100 };
-			RTransaction tr2 = new RTransaction { From = "Alice", To = "Bob", Amount = 100 };
-			Console.WriteLine(tr1);
-			Console.WriteLine(tr2);
-			Console.WriteLine($"tr1 equals to tr2: {tr1.Equals(tr2)}");
+			var b = new { Subject = "수학", Score = new int[] { 90, 80, 70, 60 } };
+
+			Console.Write($"Subject: {b.Subject}, Score: ");
+			foreach (var score in b.Score) 
+			{ 
+				Console.Write($"{score} ");				
+			}
+
+			Console.WriteLine();
 		}
 	}
 }
