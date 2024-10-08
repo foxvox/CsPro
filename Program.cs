57cf5d3e5deac934bf4406dc9448c3f037e57827
alex <windev@nate.com>
2024-10-08 19:03:46 +0900

    ^ usage in array

--- Program.cs
+++ Program.cs
@@ -1,13 +1,11 @@
-﻿using System.Xml.Schema;
-
-namespace CsPro
+﻿namespace CsPro
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
-			int[] scores = new int[5]; 
-			scores[0] = 80; scores[1] = 74; scores[2] = 81; scores[3] = 90; scores[4] = 34; 
+			int[] scores = new int[5];
+			scores[0] = 80; scores[1] = 74; scores[2] = 81; scores[^2] = 90; scores[^1] = 34; 
 
 			foreach (int score in scores) 
 				Console.WriteLine(score);
