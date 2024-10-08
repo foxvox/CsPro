a88f5e5977fd4c1ab14b419102551b26ec45dd05
alex <windev@nate.com>
2024-10-08 19:15:59 +0900

    array derived from System.Array

--- Program.cs
+++ Program.cs
@@ -4,20 +4,9 @@
 	{
 		static void Main(string[] args)
 		{
-			string[] array1 = new string[3] { "안녕", "Hello", "Halo" };  
-			Console.WriteLine("array1..."); 
-			foreach (string greeting in array1) 
-				Console.WriteLine($" {greeting}");
-
-			string[] array2 = new string[] { "안녕", "Hello", "Halo" };
-			Console.WriteLine("\narray2...");
-			foreach (string greeting in array2)
-				Console.WriteLine($" {greeting}");
-
-			string[] array3 = { "안녕", "Hello", "Halo" };
-			Console.WriteLine("\narray3...");
-			foreach (string greeting in array3)
-				Console.WriteLine($" {greeting}");
+			int[] array = new int[] { 10, 30, 20, 7, 1 }; 
+			Console.WriteLine($"Type of array: {array.GetType()}"); 
+			Console.WriteLine($"Base type of array: {array.GetType().BaseType}"); 
 		}
 	}
 }
