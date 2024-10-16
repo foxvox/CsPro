c1199ea47f44edd3a579f9a5b8aa2575228c7b98
alex <windev@nate.com>
2024-10-16 23:07:41 +0900

    Action 사용법

--- Program.cs
+++ Program.cs
@@ -1,22 +1,23 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Runtime.InteropServices;
-
-namespace CsPro
+﻿namespace CsPro
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
-			Func<int> func1 = () => 10; 
-			Console.WriteLine($"func1() : {func1()}");
+			Action act1 = () => Console.WriteLine("Action()");
+			act1(); 
 
-			Func<int, int> func2 = (x) => x * 2;
-			Console.WriteLine($"func2(4) : {func2(4)}");
+			int result = 0;
+			Action<int> act2 = (x) => result = x * x;
+			act2(3);
+			Console.WriteLine($"result: {result}");
 
-			Func<double, double, double> func3 = (x, y) => x / y;
-			Console.WriteLine($"func3(22, 7) : {func3(22, 7)}"); 		
+			Action<double, double> act3 = (x, y) =>
+			{
+				double pi = x / y;
+				Console.WriteLine($"Action<T1, T2>({x}, {y}): {pi}");
+			};
+			act3(22.0, 7.0); 		
 		}
 	}
 }
