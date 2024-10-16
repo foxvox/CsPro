4c63fedfed70b7b6ae594630ff75576fb90e95bc
alex <windev@nate.com>
2024-10-17 03:22:25 +0900

    Lambda Expression

--- Program.cs
+++ Program.cs
@@ -6,26 +6,11 @@ namespace CsPro
 	{
 		static void Main(string[] args)
 		{
-			Expression const1 = Expression.Constant(1);
-			Expression const2 = Expression.Constant(2);
-
-			Expression leftExp = Expression.Multiply(const1, const2);
-
-			Expression param1 = Expression.Parameter(typeof(int)); 
-			Expression param2 = Expression.Parameter(typeof(int));
-
-			Expression rightExp = Expression.Subtract(param1, param2);
-
-			Expression exp = Expression.Add(leftExp, rightExp);
-
 			Expression<Func<int, int, int>> expression =
-				Expression<Func<int, int, int>>.Lambda<Func<int, int, int>>(exp,
-				new ParameterExpression[] { (ParameterExpression)param1, (ParameterExpression)param2 });
-
+				(a, b) => 1 * 2 + (a - b); 
 			Func<int, int, int> func = expression.Compile();
 
-			Console.WriteLine($"1 * 2 + ({7} - {8}) = {func(7, 8)}"); 
-		
+			Console.WriteLine($"1 * 2 + ({7} - {8}) = {func(7, 8)}"); 		
 		}
 	}
 }
