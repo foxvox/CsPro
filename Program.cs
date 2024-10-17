dfb5590e402b76b5e511a64dd2f3e8135b6c1315
alex <windev@nate.com>
2024-10-17 23:07:44 +0900

    메소드, 생성자, 종료자, 속성, 인덱서를 단일 식을 이용하여 구현하는 방법

--- Program.cs
+++ Program.cs
@@ -1,16 +1,55 @@
-﻿using System.Linq.Expressions; 
+﻿using System.Linq.Expressions;
+using System.Security.Cryptography.X509Certificates;
 
 namespace CsPro
 {
+	class FriendList
+	{
+		private List<string> list = new List<string>();
+
+		public void Add(string name) => list.Add(name);
+		public void Remove(string name) => list.Remove(name);
+		public void PrintAll()
+		{
+			foreach (var s in list)
+				Console.WriteLine(s);
+		}
+
+		public FriendList() => Console.WriteLine("FriendList()");
+		~FriendList() => Console.WriteLine("~FriendList()");
+
+		public int Capacity
+		{
+			get => list.Capacity;
+			set => list.Capacity = value;
+		}
+
+		public string this[int index]
+		{
+			get => list[index];
+			set => list[index] = value;
+		}
+	}
+
 	class Program
 	{
 		static void Main(string[] args)
 		{
-			Expression<Func<int, int, int>> expression =
-				(a, b) => 1 * 2 + (a - b); 
-			Func<int, int, int> func = expression.Compile();
+			FriendList obj = new FriendList();
+			obj.Add("1111");
+			obj.Add("2222");
+			obj.Add("33333333");
+			obj.Remove("1111"); 
+			obj.PrintAll();
 
-			Console.WriteLine($"1 * 2 + ({7} - {8}) = {func(7, 8)}"); 		
+			Console.WriteLine($"{obj.Capacity}");
+			Console.WriteLine($"{obj[1].Length}");
+			obj.Capacity = 10;
+			Console.WriteLine($"{obj.Capacity}");
+
+			Console.WriteLine($"{obj[0]}"); 
+			obj[0] = "Yes";
+			obj.PrintAll(); 			 		
 		}
 	}
 }
