c3b606c5c8858bb8c5e10f109f94df7f52832193
alex <windev@nate.com>
2024-10-26 22:21:03 +0900

    delegate 위임자 사용법 대리자라고도 함

--- Program.cs
+++ Program.cs
@@ -1,55 +1,34 @@
-﻿using System.Linq.Expressions;
-using System.Security.Cryptography.X509Certificates;
-
+﻿
 namespace CsPro
 {
-	class FriendList
+	delegate int MyDelegate(int a, int b);
+
+	class Calculator
 	{
-		private List<string> list = new List<string>();
-
-		public void Add(string name) => list.Add(name);
-		public void Remove(string name) => list.Remove(name);
-		public void PrintAll()
+		public int Plus(int a, int b)
 		{
-			foreach (var s in list)
-				Console.WriteLine(s);
+			return a + b;
 		}
 
-		public FriendList() => Console.WriteLine("FriendList()");
-		~FriendList() => Console.WriteLine("~FriendList()");
-
-		public int Capacity
+		public static int Minus(int a, int b)
 		{
-			get => list.Capacity;
-			set => list.Capacity = value;
-		}
-
-		public string this[int index]
-		{
-			get => list[index];
-			set => list[index] = value;
+			return a - b;
 		}
 	}
+
 
 	class Program
 	{
 		static void Main(string[] args)
 		{
-			FriendList obj = new FriendList();
-			obj.Add("1111");
-			obj.Add("2222");
-			obj.Add("33333333");
-			obj.Remove("1111"); 
-			obj.PrintAll();
+			Calculator calc = new Calculator();
+			MyDelegate Callback;
 
-			Console.WriteLine($"{obj.Capacity}");
-			Console.WriteLine($"{obj[1].Length}");
-			obj.Capacity = 10;
-			Console.WriteLine($"{obj.Capacity}");
+			Callback = new MyDelegate(calc.Plus);
+			Console.WriteLine(Callback(3, 4));
 
-			Console.WriteLine($"{obj[0]}"); 
-			obj[0] = "Yes";
-			obj.PrintAll(); 			 		
+			Callback = new MyDelegate(Calculator.Minus); 
+			Console.WriteLine(Callback(7, 5)); 			 		
 		}
 	}
 }
