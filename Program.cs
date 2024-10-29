5c3593b12632017bff12091b11881ce56fcb4ff3
alex <windev@nate.com>
2024-10-30 01:30:58 +0900

    interrupt ex

--- Program.cs
+++ Program.cs
@@ -1,29 +1,38 @@
 ﻿using System;
 using System.Runtime.CompilerServices;
-using System.Threading; 
+using System.Threading;
 
 namespace CsPro
 {
 	class Program
 	{
-		private static void PrintThreadState(ThreadState state)
-		{
-			Console.WriteLine("{0, -16}: {1}", state, (int)state);
-		}
+		private static Thread workerThread;
+		private static bool isRunning = true;
 
 		static void Main(string[] args)
 		{
-			PrintThreadState(ThreadState.Running);
-			PrintThreadState(ThreadState.StopRequested);
-			PrintThreadState(ThreadState.SuspendRequested);
-			PrintThreadState(ThreadState.Background); 
-			PrintThreadState(ThreadState.Unstarted);			
-			PrintThreadState(ThreadState.Stopped);
-			PrintThreadState(ThreadState.WaitSleepJoin);
-			PrintThreadState(ThreadState.Suspended);
-			PrintThreadState(ThreadState.AbortRequested); 
-			PrintThreadState(ThreadState.Aborted);
-			PrintThreadState(ThreadState.Aborted | ThreadState.Stopped); 
+			// 스레드 생성 및 시작
+			workerThread = new Thread(DoWork);
+			workerThread.Start();
+
+			Console.WriteLine("Press Enter to stop the worker thread...");
+			Console.ReadLine();
+
+			// 스레드 중단
+			isRunning = false;
+
+			// 스레드가 종료될 때까지 대기
+			workerThread.Join();
+			Console.WriteLine("Worker thread has stopped.");
+		}
+
+		private static void DoWork()
+		{
+			while (isRunning)
+			{
+				Console.WriteLine("Working...");
+				Thread.Sleep(1000); // 1초 대기
+			}
 		}
 	}
 }
