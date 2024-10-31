dcb1add4d2a1114bb5f53ba9c97ba5c495a90597
alex <windev@nate.com>
2024-10-31 20:10:17 +0900

    Task 비동기실행

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic; 
 using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
@@ -7,35 +8,82 @@ namespace CsPro
 {
 	class Program
 	{
+		static bool IsPrime(long number)
+		{
+			if (number < 2)
+				return false; 
+
+			if (number %2 == 0 && number != 2) 
+				return false; 
+
+			for (long i = 2; i < number; i++)
+			{
+				if (number % i == 0) 
+					return false; 
+			}
+
+			return true; 
+		}
+
 		static void Main(string[] args)
 		{
-			string srcFile = args[0];
+			long from = Convert.ToInt64(args[0]); 
+			long to = Convert.ToInt64(args[1]);
+			int taskCount = Convert.ToInt32(args[2]);
 
-			Action<object> FileCopyAction = (object state) =>
+			Func<object, List<long>> FindPrimeFunc =
+				(objRange) =>
+				{
+					long[] range = (long[])objRange;
+					List<long> found = new List<long>();
+
+					for (long i = range[0]; i < range[1]; i++)
+					{
+						if (IsPrime(i))
+							found.Add(i);
+					}
+					return found;
+				};
+
+			Task < List<long>>[] tasks = new Task<List<long>>[taskCount];
+			long currentFrom = from;
+			long currentTo = to / tasks.Length; 
+			for (int i = 0; i < tasks.Length; i++)
 			{
-				String[] paths = (String[])state;
-				File.Copy(paths[0], paths[1]);
+				Console.WriteLine("Task[{0}]: {1} ~ {2}", i, currentFrom, currentTo);
+				tasks[i] = new Task<List<long>>(FindPrimeFunc, new long[] { currentFrom, currentTo });
 
-				Console.WriteLine("TaskID: {0}, ThreadID: {1}, {2} was copied to {3}",
-					Task.CurrentId, Thread.CurrentThread.ManagedThreadId, paths[0], paths[1]);
-			};
+				currentFrom = currentTo + 1;
 
-			Task t1 = new Task(FileCopyAction, new string[] { srcFile, srcFile + ".copy1" });
+				if (i == tasks.Length - 2)
+					currentTo = to;
+				else
+					currentTo = currentTo + (to / tasks.Length); 
+			}
 
-			Task t2 = Task.Run(() =>
+			Console.WriteLine("Please press enter to start...");
+			Console.ReadLine();
+			Console.WriteLine("Started..."); 
+
+			DateTime startTime = DateTime.Now;
+
+			foreach (Task<List<long>> task in tasks)
+				task.Start(); 
+
+			List<long> total = new List<long>();
+
+			foreach (Task<List<long>> task in tasks)
 			{
-				FileCopyAction(new string[] { srcFile, srcFile + ".copy2" });
-			});  
+				task.Wait();
+				total.AddRange(task.Result.ToArray()); 
+			}
 
-			t1.Start();
+			DateTime endTime = DateTime.Now;
 
-			Task t3 = new Task(FileCopyAction, new string[] { srcFile, srcFile + ".copy3" });
+			TimeSpan elapsed = endTime - startTime;
 
-			t3.RunSynchronously(); 
-
-			t1.Wait();
-			t2.Wait();
-			t3.Wait();
+			Console.WriteLine("Prime number count between {0} and {1}: {2}", from, to, total.Count); 
+			Console.WriteLine("Elapsed time: {0}", elapsed);
 		}
 	}
 }
