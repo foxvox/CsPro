9ed7bcfd9c6db17860eb44f8bc4f3ba87300afa8
alex <windev@nate.com>
2024-11-04 01:58:12 +0900

    event 전용 delegate 사용법 메인 메서드에 대한 식본문정의

--- Program.cs
+++ Program.cs
@@ -1,31 +1,38 @@
 ﻿
 namespace CsPro 
 {
-	delegate void MyDelegate(); 
+	delegate void MyEventHandler();
 
-	class Test
+	class MyButton
 	{
-		public static void Hello()
+		public event MyEventHandler Event;
+		public void OnClick()
 		{
-			Console.WriteLine("Test 클래스의 static Hello()"); 
+			Event();
 		}
-
-		public static MyDelegate MyHello
-		{
-			get
-			{
-				return Hello; 
-			}
-		}
-
 	}
 
 	class Program
 	{
-		static void Main(string[] args)
+		void BtnClick()
 		{
-			MyDelegate hello = Test.MyHello; 
-			hello(); 
+			Console.WriteLine("버튼에서 이벤트 발생!"); 
 		}
-	}
+
+		public Program()
+		{
+			//MyButton 객체 생성 
+			MyButton btn = new ();
+
+			//버튼에서 이벤트가 발생하면 BtnClick함수가 호출되게끔 미리 할당해 둔다.   
+			btn.Event += BtnClick;
+
+			//버튼이 클릭되면 이벤트 전용 delegate Event가 호출된다. 위에서 미리 할당해 둔 BtnClick()함수가 호출된다.  
+			btn.OnClick(); 
+
+		}
+
+		//expression bodied for Main Method: 메인 메서드에 대한 식본문 정의 
+		static void Main(string[] args) => new Program();
+	}	
 }
