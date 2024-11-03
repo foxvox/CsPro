37f66d6560da9c63cb8932ed1add7891ff43c8d8
alex <windev@nate.com>
2024-11-04 00:39:30 +0900

    delegate usage

--- Program.cs
+++ Program.cs
@@ -1,78 +1,27 @@
-﻿namespace CsPro
+﻿
+namespace CsPro 
 {
+	delegate void MyDelegate(); 
+
 	class Test
 	{
-		int[] array = [10, 20, 30, 40, 50];
-		int index;
- 
-		public int this[int index]
+		public void Hello()
 		{
-			get
-			{
-				if (index >= 0 && index < array.Length)
-				{
-					return array[index];
-				}
-				else
-				{
-					return -1;
-				}
-			}
-			set
-			{
-				if (index >= 0 && index < array.Length)
-				{
-					array[index] = value;
-				}
-				else
-				{
-					Console.WriteLine("array 범위를 벗어났습니다.");
-				}
-			}
+			Console.WriteLine("Hello World!"); 
 		}
 
-		public int Array
-		{
-			get
-			{
-				return array[index];
-			}
-			set
-			{
-				array[index] = value;
-			}
-		}
-		
-		public int ArrayLength
-		{
-			get
-			{
-				return array.Length;
-			}
-		}
-
-		public int Index
-		{
-			set
-			{
-				index = value; 
-			}
-		}
 	}
 
 	class Program
 	{
 		static void Main(string[] args)
 		{
-			Test test = new Test(); 
-			test[2] = 700; 
-			for (int i = 0; i < test.ArrayLength; i++)
-			{
-				Console.WriteLine($"{test[i]}"); 
-			}
+			Test t = new();
+			t.Hello(); 
 
-			test.Index = 2; 
-			Console.WriteLine(test.Array); 
+			//메서드의 참조가 들어감 
+			MyDelegate hello = t.Hello;
+			hello(); 
 		}
 	}
 }
