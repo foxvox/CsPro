50bcc340ce138ae0a1f53f0f22642134b6ae5b7c
alex <windev@nate.com>
2024-11-05 01:41:47 +0900

    file 존재 확인

--- Program.cs
+++ Program.cs
@@ -1,38 +1,22 @@
 ﻿
+using System.IO; 
+
 namespace CsPro 
 {
-	delegate void MyEventHandler();
-
-	class MyButton
-	{
-		public event MyEventHandler Event;
-		public void OnClick()
-		{
-			Event();
-		}
-	}
-
 	class Program
 	{
-		void BtnClick()
+		static void Main(string[] args)
 		{
-			Console.WriteLine("버튼에서 이벤트 발생!"); 
-		}
+			FileInfo fi = new FileInfo(@"C:/csharpex/test.txt");
 
-		public Program()
-		{
-			//MyButton 객체 생성 
-			MyButton btn = new ();
-
-			//버튼에서 이벤트가 발생하면 BtnClick함수가 호출되게끔 미리 할당해 둔다.   
-			btn.Event += BtnClick;
-
-			//버튼이 클릭되면 이벤트 전용 delegate Event가 호출된다. 위에서 미리 할당해 둔 BtnClick()함수가 호출된다.  
-			btn.OnClick(); 
-
-		}
-
-		//expression bodied for Main Method: 메인 메서드에 대한 식본문 정의 
-		static void Main(string[] args) => new Program();
+			if (fi.Exists)
+			{
+				Console.WriteLine("파일이 존재합니다.");
+			}
+            else
+            {
+				Console.WriteLine("파일이 존재하지 않습니다."); 
+            }
+        }
 	}	
 }
