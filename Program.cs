458752ef0a7573a05ed53cb9c4d558b867832efa
alex <windev@nate.com>
2024-11-05 02:44:55 +0900

    StreamWriter를 이용한 파일 쓰기

--- Program.cs
+++ Program.cs
@@ -1,40 +1,14 @@
 ﻿
-using System.IO; 
-
 namespace CsPro 
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
-			FileInfo fi = new FileInfo(@"C:/csharpex/test.txt");
-			DirectoryInfo di = new DirectoryInfo(@"c:/csharpex"); 
-
-			if (fi.Exists)
-			{
-				Console.WriteLine("파일이 존재합니다.");
-			}
-			else
-			{
-				Console.WriteLine("파일이 존재하지 않습니다.");
-			}
-
-			if (di.Exists)
-			{
-				Console.WriteLine("디렉토리가 존재합니다.");
-				Console.WriteLine(di.Attributes);
-				Console.WriteLine(di.CreationTime); 
-				Console.WriteLine(di.Exists);
-				Console.WriteLine(di.FullName);
-				Console.WriteLine(di.LastAccessTime);
-				Console.WriteLine(di.Name);
-				Console.WriteLine(di.Parent);
-				Console.WriteLine(di.Root);
-			}
-			else
-			{
-				Console.WriteLine("디렉토리가 존재하지 않습니다."); 
-			}
+			StreamWriter sw = new StreamWriter(new FileStream(@"c:/csharpex/write.txt", FileMode.Create));
+			sw.WriteLine("Hello World!");
+			sw.Close();
+			Console.WriteLine("파일에 정상적으로 문자열을 추가하였습니다."); 
 		}
 	}
 }
