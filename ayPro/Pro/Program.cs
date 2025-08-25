55081b93286a94f3243ee9f90a81b5ca6da24593
foxvox <jesse23@naver.com>
2025-08-25 15:41:53 +0900

    cs 기초
    기초

---
+++ ayPro/Pro/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Pro
+{
+    internal class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            /*
+            #region 박싱
+            // 값형식의 데이터를 참조형식으로 변환하는 과정입니다. 
+            int value = 100; 
+            object obj1 = value;             
+
+            Console.WriteLine("value: " + value);
+            Console.WriteLine("obj: " + obj1);
+
+            #endregion
+
+            #region 언박싱 
+            // 참조형식의 데이터를 값형식으로 변환하는 과정입니다. 
+            int box = (int)obj1;
+
+            Console.WriteLine("\nbox: " + box);
+
+            #endregion 
+            */
+
+            Utility util = new(); 
+
+            int a = 100; 
+            int b = 200;
+            int minus = -400; 
+
+            Console.WriteLine("Swap 전 => a: " + a + "  " + "b: " + b); 
+            Utility.Swap(ref a, ref b); 
+            Console.WriteLine("Swap 후 => a: " + a + "  " + "b: " + b);
+
+            int abs = Utility.Absolute(in minus);  
+            Console.WriteLine("abs: " + abs); 
+        }
+    }
+}
---
+++ ayPro/Pro/Utility.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pro
+{
+    internal class Utility
+    {
+        private int data;
+
+        public void Pause()
+        {
+            Console.WriteLine("Pause...");
+        }
+
+        public static void Swap(ref int x, ref int y)
+        {
+            int temp = x;
+            x = y;
+            y = temp;
+        }
+
+        public static int Absolute(in int value) 
+        {
+            int temp = value;
+
+            if (value < 0)
+                temp = -value;
+            
+            return temp; 
+            
+        }
+    }
+}
